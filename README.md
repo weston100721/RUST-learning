d639c571cd83c4bba51ebfd6dcf6ee0e75021f06
zhaoliang <weston_contribute@163.com>
2019-08-19 15:52:04 +0800

    update readme.md

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 1. [《The rust programming language》](https://doc.rust-lang.org/book/title-page.html)
 
+2. [《Rust程序语言设计》中文版](https://kaisery.github.io/trpl-zh-cn/foreword.html)
+
--- projects/common_programming_concepts/src/main.rs
+++ projects/common_programming_concepts/src/main.rs
@@ -7,6 +7,23 @@ fn main() {
     control_flow_test();
     loop_test();
     while_test();
+
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!("{}",user1.email);
+
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
 }
 
 fn control_flow_test(){
