91c688f263b3dea174e710934efa24862c7ed4c9
zhaoliang <weston_contribute@163.com>
2019-12-22 21:24:19 +0800

    error handling

---
+++ projects/error-handling/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::result::Result::Ok;
+use std::result::Result::Err;
+use std::io::ErrorKind;
+use std::result::Result;
+use std::string::String;
+use std::io;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn main() {
+    // panic!("crash and burn");
+
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+}
